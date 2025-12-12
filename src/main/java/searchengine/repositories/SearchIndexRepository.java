4f95f9166ac67cc6c4b1be7be7330427d60f558d
kshmailov <kirillshmailov@gmail.com>
2025-12-13 00:26:34 +0500

    Устранена ошибка удаления страницы jakarta.persistence.TransactionRequiredException: No EntityManager with actual transaction available for current thread - cannot reliably process 'remove' call при повторной индексации, по причине того, что операция производилась вне транзакции

--- src/main/java/searchengine/repositories/SearchIndexRepository.java
+++ src/main/java/searchengine/repositories/SearchIndexRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import searchengine.model.SearchIndexEntity;
 @Repository
 public interface SearchIndexRepository extends JpaRepository<SearchIndexEntity,Integer> {
+    @Transactional
     void deleteAllByPageId(int pageId);
     @Query("""
     SELECT SUM(ie.lemmaRank)
